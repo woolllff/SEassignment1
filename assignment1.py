aa6188bb119baa406dee5b1d9b9e804edf84ac6a
gandharv <gandharv.suri@iiitb.org>
2020-08-14 10:36:57 +0530

    First Commit

---
+++ assignment1.py
@@ -0,0 +1,20 @@
+def addition(a,b):
+	return a+b
+
+def subtraction(a,b):
+	return a-b;
+
+
+if __name__ == "__main__":
+	a = 3
+	b = 5
+	print(addition(a,b))
+	print(subtraction(a,b))
+
+
+'''
+clone from github
+Create new branches for Multiplication and Division
+merge with master
+push to github 
+'''
